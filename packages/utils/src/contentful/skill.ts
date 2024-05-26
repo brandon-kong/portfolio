7de148e8ddd238de88df70dd5dcee0e539b3dcda
Brandon Kong <kongbrandon0@gmail.com>
2024-05-26 15:33:24 -0500

    feat: ordered skills

--- packages/utils/src/contentful/skill.ts
+++ packages/utils/src/contentful/skill.ts
@@ -1,9 +1,16 @@
 import { SkillEntrySkeleton, TypeSkill } from '../../types/skill';
 import { cfClient } from './cf-client';
 
+const order = ['Programming Language', 'Tool', 'Database', 'Framework'];
+
 export const fetchSkills = async (): Promise<TypeSkill[]> => {
     const entries = await cfClient.getEntries<SkillEntrySkeleton>({
         content_type: 'skill',
     });
-    return entries.items;
+
+    const ordered = entries.items.sort((a, b) => {
+        return order.indexOf(a.fields.type) - order.indexOf(b.fields.type);
+    });
+
+    return ordered;
 };
