2d4333cbd9e6f9bb6ba6e3cd3838e80358730de8
brandon-kong <95515854+brandon-kong@users.noreply.github.com>
2024-08-16 12:08:07 -0500

    feat: revalidated cache after 1 hour, fixed no link in projects button

--- apps/personal/src/app/layout.tsx
+++ apps/personal/src/app/layout.tsx
@@ -7,6 +7,8 @@ import { ThemeProvider } from '@repo/ui/providers';
 import { Sidebar, Footer, Navbar } from '@repo/ui/navigation';
 import { cn } from '@repo/utils';
 
+export const revalidate = 3600
+
 const inter = Inter({ subsets: ['latin'] });
 const sourceCodePro = Source_Code_Pro({
     subsets: ['latin'],
